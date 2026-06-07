9a5799972b0ee0158fc877f7e5a423053de24cf0
啃 竹子 <bytfr@grmine.onmicrosoft.com>
2026-06-07 15:59:19 +0800

    修复主页空侧边栏占位问题

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,13 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useParams } from 'react-router-dom';
 import Navbar from './Navbar';
 import Sidebar from './Sidebar';
 import SearchDialog from './SearchDialog';
 import { useSidebarStore } from '@/stores/sidebarStore';
+import { getProject } from '@/data/projects';
 
 export default function Layout() {
+  const { project: projectId } = useParams();
+  const hasSidebar = !!projectId && !!getProject(projectId);
   const sidebarOpen = useSidebarStore((s) => s.isOpen);
   const setSidebarOpen = useSidebarStore((s) => s.setOpen);
 
@@ -12,10 +15,12 @@ export default function Layout() {
     <div className="app-layout">
       <Navbar />
       <div className="app-body">
-        <div className="sidebar-wrapper" data-open={sidebarOpen}>
-          <Sidebar />
-        </div>
-        {sidebarOpen && (
+        {hasSidebar && (
+          <div className="sidebar-wrapper" data-open={sidebarOpen}>
+            <Sidebar />
+          </div>
+        )}
+        {hasSidebar && sidebarOpen && (
           <div
             className="sidebar-overlay"
             onClick={() => setSidebarOpen(false)}
